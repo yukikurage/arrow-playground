133f5e986766ed7be37c8d65da982d886da79438
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-10-18 17:53:13 +0900

    add main

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,18 @@ module Main where
 
 import Prelude
 
+import ArrowPlayground.EventMachine (EventSource, merge)
 import Effect (Effect)
 import Effect.Console (log)
 
 main :: Effect Unit
 main = do
   log "🍝"
+
+counter :: EventSource Unit -> EventSource Unit
+counter start = let
+  go :: EventSource Int -> EventSource Int
+  go feedback = let
+    begin = map (const 0) start
+    next = delay $  merge begin (map (add 1) feedback)
+    in next
